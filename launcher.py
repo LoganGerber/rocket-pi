e16289c62cb3ed025ed8a9c07ab18b714dbd378b
Logan Gerber <logangerber1014@yahoo.com>
2019-08-23 03:33:25 +0100

    Fix file ending typo

--- launcher.py
+++ launcher.py
@@ -4,6 +4,11 @@ import os
 python_dir = '/home/pi/rocket/venv/bin/python3'
 process_dir = '/home/pi/rocket/processes'
 
-for filename in [name for name in os.listdir(process_dir) if os.path.isfile(os.path.join(process_dir, name)) and name.endswith('.pi')]:
+launched_count = 0
+
+for filename in [name for name in os.listdir(process_dir) if os.path.isfile(os.path.join(process_dir, name)) and name.endswith('.py')]:
     subprocess.Popen([python_dir, os.path.join(process_dir, filename)])
+    launched_count += 1
+
+print(launched_count)
 
