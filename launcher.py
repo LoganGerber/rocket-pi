64a329dd4fa4200167ca4f05c30e6a58d7c2e012
Logan Gerber <logangerber1014@yahoo.com>
2019-08-24 10:57:07 +0100

    Rename processes directory to modules

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 python_dir = '/home/pi/rocket/venv/bin/python3'
-process_dir = '/home/pi/rocket/processes'
+process_dir = '/home/pi/rocket/modules'
 
 for filename in [name for name in os.listdir(process_dir) if os.path.isfile(os.path.join(process_dir, name)) and name.endswith('.py')]:
     subprocess.Popen([python_dir, os.path.join(process_dir, filename)])
