b1c574a90e3e59f3afd7cd08ff1f219f5a0a4943
Logan Gerber <logangerber1014@yahoo.com>
2019-08-24 10:59:36 +0100

    Made directory paths relative to the launcher script location

--- launcher.py
+++ launcher.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
 
-python_dir = '/home/pi/rocket/venv/bin/python3'
-process_dir = '/home/pi/rocket/modules'
+script_dir = os.path.dirname(os.path.realpath(__file__))
+python_dir = os.path.join(script_dir, 'venv/bin/python3')
+process_dir = os.path.join(script_dir, 'modules')
 
 for filename in [name for name in os.listdir(process_dir) if os.path.isfile(os.path.join(process_dir, name)) and name.endswith('.py')]:
     subprocess.Popen([python_dir, os.path.join(process_dir, filename)])
