f9558319288e34587d1fdbca98d478e8f1b56317
Logan Gerber <logangerber1014@yahoo.com>
2019-08-29 06:12:23 +0100

    Blink LED when video breaks, turn LED on when recording, and press button to begin recording

--- modules/record_video.py
+++ modules/record_video.py
@@ -3,41 +3,65 @@ import datetime
 import multiprocessing as mp
 
 import picamera
-import gpiozero
+from gpiozero import LED, Button
 
 def RecordVideo(stop_event):
-    # Set up camera
-    cam = picamera.PiCamera(resolution=(1920,1080), framerate=30)
+    led = LED(26)
+    try:
+        button = Button(17, hold_time=0.25, bounce_time=0.01)
 
-    # Get the current date and time to use for creating the log directory and file
-    current_date = str(datetime.datetime.now()).split(' ')
+        # Set up camera
+        cam = picamera.PiCamera(resolution=(1920,1080), framerate=30)
 
-    # Remove the decimals from the seconds in the current time
-    current_date[1] = current_date[1].split('.')[0]
+        # Get the current date and time to use for creating the log directory and file
+        current_date = str(datetime.datetime.now()).split(' ')
 
-    # Replace the ':' with '-' in the current time to make it a valid directory name
-    current_date[1] = current_date[1].replace(':', '-')
+        # Remove the decimals from the seconds in the current time
+        current_date[1] = current_date[1].split('.')[0]
 
-    # Ensure the log directory exists
-    log_dir = os.path.dirname(os.path.realpath(__file__))
-    log_dir = os.path.join(log_dir, '../logs/video/{}'.format(current_date[0]))
+        # Replace the ':' with '-' in the current time to make it a valid directory name
+        current_date[1] = current_date[1].replace(':', '-')
 
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+        # Ensure the log directory exists
+        log_dir = os.path.dirname(os.path.realpath(__file__))
+        log_dir = os.path.join(log_dir, '../logs/video/{}'.format(current_date[0]))
 
-    # Define Filename generating function
-    def CreateFilename(file_number):
-        file_name = '{:03d}.h264'.format(file_number)
-        return os.path.join(log_dir, file_name)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
-    record_number = 0
-    # Video creation loop
-    while not stop_event.is_set():
-        camera.start_recording(CreateFilename(recording_file_path, record_number))
-        camera.wait_recording(10)
-        record_number += 1
+        # Define Filename generating function
+        def CreateFilename(file_number):
+            file_name = '{:03d}.h264'.format(file_number)
+            return os.path.join(log_dir, file_name)
 
-    camera.stop_recording()
+        should_record = False
+        button_held = False
+
+        record_number = 0
+        led.on()
+        # Video creation loop
+        while not stop_event.is_set():
+            if should_record:
+                led.on()
+                camera.start_recording(CreateFilename(recording_file_path, record_number))
+                camera.wait_recording(10)
+                record_number += 1
+            else:
+                led.off()
+                button.wait_for_press(1)
+
+            if button.is_pressed and not button_held:
+                should_record = not should_record
+                button_held = True
+            elif not button.is_pressed and button_held:
+                button_held = False
+
+        camera.stop_recording()
+
+    except:
+        led.blink()
+
+        stop_event.wait()
 
 def DoNothing(signal, frame):
     pass
